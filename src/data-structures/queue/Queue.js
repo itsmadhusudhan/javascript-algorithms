acb0f8fd1d0de20816bd20900c85f5edfe3e219c
itsmadhusudhan <madhusudhan1863@gmail.com>
2018-05-29 22:49:38 +0530

    added queue data structure and tests

---
+++ src/data-structures/queue/Queue.js
@@ -0,0 +1,29 @@
+import LinkedList from "../linked-list/LinkedList";
+
+export default class Queue{
+  constructor(){
+    this.linkedList=new LinkedList()
+  }
+  isEmpty(){
+    return !this.linkedList.tail;
+  }
+  peek(){
+    if(!this.linkedList.head){
+      return null;
+    }
+    return this.linkedList.head.value;
+  }
+
+  enqueue(value){
+    this.linkedList.append(value);
+  }
+  dequeue(){
+    const removeHead=this.linkedList.deleteHead();
+    return removeHead?removeHead.value:null;
+  }
+
+  toString(callback){
+    return this.linkedList.toString(callback);
+  }
+
+}
---
+++ src/data-structures/queue/README.md
@@ -0,0 +1,13 @@
+# Queue
+
+Queue is a data structure in which the element is add at the rear end.
+
+It follows `First In First Out (FIFO)`.
+
+A Queue
+
+1. Can be empty
+2. Can get the first value with peek() method
+3. Can enqueue a value/add value to the queue
+4. Can dequeue a value from queue/take value from queue
+5. Can convert the values in the queue to a string
